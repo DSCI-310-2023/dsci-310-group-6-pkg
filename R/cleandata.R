b73efa8a93c16461211208cea4aa3b98ff23ccfc
Davis Li <davismxli@gmail.com>
2023-04-05 17:20:31 -0700

    updated DESCRIPTION file

--- R/cleandata.R
+++ R/cleandata.R
@@ -1,3 +1,17 @@
+#' Reads data and unselect the specified columns
+#'
+#' Return the data frame without the specified columns from the input_path
+#'
+#' @param input_path A quoted path to the data file
+#' @param output_path A quoted destination path
+#' @param cols A list of quoted column name that we want to unselect
+#'
+#' @return A data frame excluding the specified columns
+#'
+#' @export
+#'
+#' @examples
+
 clean_data <- function(input_path, output_path = NULL, cols = NULL) {
   if(!is.character(input_path)) {
     stop("`read_data` expects a quoted path of the data file as the first input")
