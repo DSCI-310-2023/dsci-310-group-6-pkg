e2f9443dd17f6d93f78ef610093c29a8aff45fe6
Davis Li <davismxli@gmail.com>
2023-04-10 19:10:30 -0700

    increased test coverage for clean data

--- tests/testthat/test-clean_data.R
+++ tests/testthat/test-clean_data.R
@@ -23,3 +23,19 @@ are passed to `file_path` and `cols` arguments", {
   expect_error(clean_data(hi, output_path = NULL, c("x")))
   expect_error(clean_data(file_path, y))
 })
+
+
+# Input_path is not a character
+test_that("clean_data throws an error when input_path is not a character", {
+  expect_error(clean_data(123), "`clean_data` expects a quoted path of the data file as the first input")
+})
+
+# Test case 4: output_path is specified
+test_that("clean_data writes data to output_path when specified", {
+  data <- data.frame(x = c(1,2,3), y = c(4,5,6), z = c(7,8,9))
+  write.csv(data, file = "test_data.csv", row.names = FALSE)
+  temp_file <- tempfile(fileext = ".csv")
+  clean_data("test_data.csv", output_path = temp_file)
+  expect_true(file.exists(temp_file))
+  file.remove("test_data.csv")
+})
