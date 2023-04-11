f589380a7a870d5fc3f7e96f1afce0b89a4f5c49
Davis Li <davismxli@gmail.com>
2023-04-10 17:52:33 -0700

    added rmd

--- tests/testthat/test-clean_data.R
+++ tests/testthat/test-clean_data.R
@@ -1,24 +1,3 @@
-# file_path <- here::here('data/sample_data.csv')
-#
-# # Test that the function returns a data frame
-# test_that("`clean_data` should return a data frame", {
-#   expect_equal(class(clean_data(file_path, output_path = NULL, cols = NULL)), "data.frame")
-# })
-#
-# # Test that the function returns the right data frame with 2 columns
-# test_that("`clean_data` returns a dataframe with 2 columns", {
-#   expect_equal(ncol(clean_data(file_path, output_path = NULL,c("x"))), 2)
-# })
-#
-# # `read_data` should throw an error when incorrect types are passed to `file_path` and `cols` arguments"
-# test_that("`clean_data` should throw an error when incorrect types
-# are passed to `file_path` and `cols` arguments", {
-#   expect_error(clean_data(hi, output_path = NULL, c("x")))
-#   expect_error(clean_data(file_path, y))
-# })
-
-library(testthat)
-
 # Test if function returns expected data frame
 test_that("clean_data() returns expected data frame", {
   data <- data.frame(x = c(1,2,3), y = c(4,5,6), z = c(7,8,9))
